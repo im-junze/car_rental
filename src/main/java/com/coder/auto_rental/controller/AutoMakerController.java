4fa9ccb08ab26b7b32a585bdec4e77e5677e18fd
猪墩墩 <1921669890@qq.com>
2024-10-24 15:42:56 +0800

    厂商模块新增删除，厂商功能完成

--- src/main/java/com/coder/auto_rental/controller/AutoMakerController.java
+++ src/main/java/com/coder/auto_rental/controller/AutoMakerController.java
@@ -1,12 +1,18 @@
 package com.coder.auto_rental.controller;
 
+import cn.hutool.extra.pinyin.PinyinUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.coder.auto_rental.entity.AutoMaker;
 import com.coder.auto_rental.service.IAutoMakerService;
+import com.coder.auto_rental.utils.PinYinUtils;
 import com.coder.auto_rental.utils.Result;
 import jakarta.annotation.Resource;
 import org.apache.poi.sl.usermodel.AutoShape;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * <p>
@@ -32,5 +38,20 @@ public class AutoMakerController {
 
     }
 
+    @DeleteMapping("/{ids}")
+        public Result deleteById(@PathVariable String ids){
+         List<Integer> list =  Arrays.stream(ids.split(",")).map(Integer::parseInt).toList();
+        return autoMakerAutoMakerService.removeByIds(list)?Result.success():Result.fail();
+    }
 
+    @PostMapping
+    public Result save(@RequestBody AutoMaker autoMaker){
+        autoMaker.setOrderLetter(PinYinUtils.getPinYin(autoMaker.getName()));
+        return autoMakerAutoMakerService.saveOrUpdate(autoMaker)?Result.success():Result.fail();
+    }
+    @PutMapping
+    public Result update(@RequestBody AutoMaker autoMaker){
+        autoMaker.setOrderLetter(PinYinUtils.getPinYin(autoMaker.getName()));
+        return autoMakerAutoMakerService.updateById(autoMaker)?Result.success():Result.fail();
+    }
 }
